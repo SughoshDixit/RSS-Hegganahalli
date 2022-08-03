8449f5a0174cf6dc4e4ed4a254be83c53c0cc9b9
Your Name <you@example.com>
2022-08-03 19:08:09 +0530

    Updates

---
+++ precache-manifest.bf815e4dce6776b310befdc83eaa9117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d7a33c805d8e576bc4868d59f00f9f",
+    "url": "/RSS_Hegganahalli/index.html"
+  },
+  {
+    "revision": "e6404407c9b498738818",
+    "url": "/RSS_Hegganahalli/static/css/main.1e18207e.chunk.css"
+  },
+  {
+    "revision": "fb4437c29dff88d270e1",
+    "url": "/RSS_Hegganahalli/static/js/2.2972f6bd.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/RSS_Hegganahalli/static/js/2.2972f6bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6404407c9b498738818",
+    "url": "/RSS_Hegganahalli/static/js/main.831963eb.chunk.js"
+  },
+  {
+    "revision": "375c8e82f80f7183fb03",
+    "url": "/RSS_Hegganahalli/static/js/runtime-main.4520554b.js"
+  }
+]);
