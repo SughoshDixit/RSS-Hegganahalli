1a4f21e22a08a25be3783671ec6e36b7ef4f7199
Your Name <you@example.com>
2022-08-03 19:02:26 +0530

    First Commit

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "Z2hwXzR4NUxOWGtTMWc0TzZhWUZXTGp3V2xDM1F0VHk1YTJoamJ1Yg==",
+
+  // Your Github UserName
+  githubUserName: "SughoshDixit",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "RSS Heggamahalli Blog",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.667b7164fab3d259',
+  OAuthSecret: '0b751ff4d10b65614e984c8cb58e15cceca812d5',
+
+  // Your Personal Blog Title
+  title : "ರಾಷ್ಟ್ರೀಯ ಸ್ವಯಂ ಸೇವಕ ಸಂಘ ಹೆಗ್ಗನಹಳ್ಳಿ" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Rashtriya Swayam Sevak Sangh Hegganahalli",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
